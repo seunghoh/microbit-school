3490a7d060fdfb3af160c383e5ed1c7bf616c994
Seungho Park <hohpark@Antares-MacBook-Pro.local>
2022-02-03 17:40:25 +0900

    Add project09 Making fan with temperature sensor

---
+++ project09-fan/microbit-project09.js
@@ -0,0 +1,16 @@
+let temp = 0
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
+basic.forever(function () {
+    temp = input.temperature()
+    if (temp >= 25) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+    basic.showNumber(temp)
+})
