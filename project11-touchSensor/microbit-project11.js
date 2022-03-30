b887c2451639c4ec55d673ea1b938e1697bdfa75
Seungho Park <hohpark@Antares-MacBookPro-M1.local>
2022-03-30 22:46:53 +0900

    Add project11 source codes

---
+++ project11-touchSensor/microbit-project11.js
@@ -0,0 +1,18 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        # . . . #
+        . # # # .
+        `)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . # . #
+        # . . . #
+        . # # # .
+        `)
+})
